6ec971adb074c4735920d8f3bb4054f5d9d8bdab
Bharadwaj <bharadwaja307@gmail.com>
2023-11-26 17:10:30 +0530

    first commit

---
+++ Filter app/main.js
@@ -0,0 +1,63 @@
+var image;
+var canvas1;
+var canvas2;
+
+function uploadimg() {
+ canvas1=document.getElementById("canvas1");
+ var img=document.getElementById("inputfile")
+ image=new SimpleImage(img);
+ image.drawTo(canvas1);
+}
+
+function checkfilter(clr) {
+    if (clr.target.value === "purple"){
+      purplefilt();
+    }
+    else if(clr.target.value === "Green"){
+        greenfilt();
+    }
+    else if(clr.target.value === "Blue"){
+        bluefilt();
+    }
+    else if(clr.target.value === "fade"){
+        xfilt();
+    }
+  }
+
+ function purplefilt(){
+    var newimg=image;
+    canvas2=document.getElementById("canvas2");
+    for(var pixel of newimg.values()){
+        pixel.setGreen(0);
+    }
+    newimg.drawTo(canvas2);
+ }
+ 
+ function greenfilt(){
+    var newimg=image;
+    canvas2=document.getElementById("canvas2");
+    for(var pixel of newimg.values()){
+        pixel.setGreen(255);
+    }
+    newimg.drawTo(canvas2);
+ }
+
+ function bluefilt(){
+    var newimg=image;
+    canvas2=document.getElementById("canvas2");
+    for(var pixel of newimg.values()){
+        pixel.setBlue(255);
+    }
+    newimg.drawTo(canvas2);
+ }
+
+ function xfilt(){
+    var newimg=image;
+    canvas2=document.getElementById("canvas2");
+    for(var pixel of newimg.values()){
+        pixel.setAlpha(100);
+    }
+    newimg.drawTo(canvas2);
+ }
+
+ 
